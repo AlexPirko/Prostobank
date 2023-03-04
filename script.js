c3084a85937dbe9b2f17d65f7a7ba4a05feb0100
Alex Pirko <youzhny_foto@gmail.com>
2023-03-04 14:59:41 +0200

    feat: add new functions

--- script.js
+++ script.js
@@ -6,9 +6,17 @@
 const modalWindow = document.querySelector('.modal-window');
 const overlay = document.querySelector('.overlay');
 const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
-const btnsOpenModalWindow = document.querySelectorAll(
-  '.btn--show-modal-window'
-);
+const btnsOpenModalWindow = document.querySelectorAll('.btn--show-modal-window');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const allLinks = document.querySelector('.nav__links');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabContents = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
+
+
+//Modal window
 
 const openModalWindow = function (e) {
   e.preventDefault();
@@ -33,10 +41,7 @@ document.addEventListener('keydown', function (e) {
 });
 
 
-// Scroll to function
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// Scroll to page
 
 btnScrollTo.addEventListener('click', function(e) {
   // const section1Coords = section1.getBoundingClientRect();
@@ -47,3 +52,52 @@ btnScrollTo.addEventListener('click', function(e) {
   // });
   section1.scrollIntoView({behavior: 'smooth'})
 });
+
+
+// Smooth page navigation
+
+allLinks.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const href = e.target.getAttribute('href');
+    document.querySelector(href).scrollIntoView({
+      behavior: 'smooth'
+    });
+  } 
+});
+
+
+// вкладки
+
+tabContainer.addEventListener('click', function(e) {
+  const clickedButton = e.target.closest('.operations__tab');
+
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  clickedButton.classList.add('operations__tab--active');
+
+  tabContents.forEach(content => content.classList.remove('operations__content--active'));
+  document.querySelector(`.operations__content--${clickedButton.dataset.tab}`).classList.add('operations__content--active');
+});
+
+
+// Анимация потускнения в хедер
+
+const navLinksHoverAnimation = function(e) {
+  if(e.target.classList.contains('nav__link')) {
+    const linkOver = e.target;
+    const siblingLinks = linkOver.closest('.nav__links').querySelectorAll('.nav__link');
+    const logo = linkOver.closest('.nav').querySelector('img');
+    const logoText = linkOver.closest('.nav').querySelector('.nav__text');
+    
+    siblingLinks.forEach(el => {
+      if(el != linkOver) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+    logoText.style.opacity = this;
+  }
+};
+
+
+nav.addEventListener('mouseover', navLinksHoverAnimation.bind(0.4));
+
+nav.addEventListener('mouseout', navLinksHoverAnimation.bind(1));
